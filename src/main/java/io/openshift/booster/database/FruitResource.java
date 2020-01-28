97894d031bf3f70ff15857555ab0e3e4db202d9c
veer <veer.muchandi@gmail.com>
2020-01-28 02:30:32 +0000

    added search by name

--- src/main/java/io/openshift/booster/database/FruitResource.java
+++ src/main/java/io/openshift/booster/database/FruitResource.java
@@ -29,6 +29,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -47,6 +48,19 @@ public class FruitResource {
     public Fruit[] get() {
         return em
                 .createNamedQuery("Fruits.findAll", Fruit.class)
+                .getResultList()
+                .toArray(new Fruit[0]);
+    }
+
+    @GET
+    @Produces("application/json")
+    public Fruit[] getByName(@QueryParam("name") String name) {
+        if (name == null) {
+            return get();
+        } else
+        return em
+                .createNamedQuery("Fruits.findByName", Fruit.class)
+                .setParameter("name", name)
                 .getResultList()
                 .toArray(new Fruit[0]);
     }
