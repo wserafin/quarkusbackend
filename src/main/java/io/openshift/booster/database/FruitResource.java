c2fc889fa01bd09b826fd4730c73138b6b4eb25b
VeerMuchandi <veer.muchandi@gmail.com>
2020-02-25 17:30:27 -0500

    Revert "added query by name"
    This reverts commit 17a818d041c51e1b282c65fdf82c7f9e7e1ebfae.

--- src/main/java/io/openshift/booster/database/FruitResource.java
+++ src/main/java/io/openshift/booster/database/FruitResource.java
@@ -29,7 +29,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -63,18 +62,6 @@ public class FruitResource {
         }
 
         return Response.ok(entity).status(200).build();
-    }
-
-    @GET
-    @Produces("application/json")
-    public Fruit[] getByName(@QueryParam("name") String name) {
-
-        if (name == null) return get();
-        return em
-                .createNamedQuery("Fruits.findByName", Fruit.class)
-                .setParameter("name", name)
-                .getResultList()
-                .toArray(new Fruit[0]);
     }
 
 
