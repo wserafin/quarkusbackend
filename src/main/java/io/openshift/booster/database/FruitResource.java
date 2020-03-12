f041a64a30140b608d82e44cda64ca1bfc488a85
veermuchandi <root@workspaceeyqsja3uznszinb0.workspace-94448dbd7-679z4>
2020-03-12 20:47:49 +0000

    added search by name

--- src/main/java/io/openshift/booster/database/FruitResource.java
+++ src/main/java/io/openshift/booster/database/FruitResource.java
@@ -29,6 +29,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**
@@ -62,6 +63,20 @@ public class FruitResource {
         }
 
         return Response.ok(entity).status(200).build();
+    }
+
+    @GET
+    @Produces("application/json")
+    public Fruit[] getByName(@QueryParam("name") String name) {
+
+        if (name == null) {
+            return get();
+        } else
+        return em
+                .createNamedQuery("Fruits.findByName", Fruit.class)
+                .setParameter("name", name)
+                .getResultList()
+                .toArray(new Fruit[0]);
     }
 
 
